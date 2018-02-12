a2c6c46027e258f97711bb9c9ea413a5c6546378
Rizal Hilman <rizal.drawrs@gmail.com>
2018-02-12 11:51:45 +0700

    Komentar petunjuk lebih spesifik

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,8 @@
 <?php
+// mengkoneksikan ke database
+$server = "localhost";
+$server_username = "root";
+$server_password = "";
+$database_name =  "db_portal_berita";
 
-$conn = new Mysqli("localhost", "root", "", "db_portal_berita");
+$conn = new Mysqli($server, $server_username, $server_password, $database_name);
--- tampil_berita.php
+++ tampil_berita.php
@@ -1,21 +1,30 @@
 <?php
+// include file koneksi
 require 'koneksi.php';
-
-$sql_get_berita = "SELECT * FROM tb_berita";
+// buat QUery perintah untuk menampilkan semua data
+// Secara Descending berdasarkan ID
+$sql_get_berita = "SELECT * FROM tb_berita ORDER BY id DESC";
 $query = $conn->query($sql_get_berita);
 
+// Variable penampung array sementara
 $response_data = null;
 
 while ($data = $query->fetch_assoc()) {
+	// tambahkan data yg di seleksi ke dalam array
 	$response_data[] = $data;
 }
 
+// Cek apakah datanya null ?
 if (is_null($response_data)) {
+	// jika ya, buat status untuk response jadi false
 	$status = false;
 } else {
+	// jika tidak, buat status untuk response jadi true
 	$status = true;
 }
+// Set type header response ke Json
 header('Content-Type: application/json');
-
+// Bungkus data dalam array
 $response = ['status' => $status, 'berita' => $response_data];
+// tampilkan dan convert ke format json
 echo json_encode($response);
